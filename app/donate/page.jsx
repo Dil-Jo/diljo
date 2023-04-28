87a53c9e13e90133e12e2963f268c5328da3dd84
HussainSyed268 <snaqvi.bscs21seecs@seecs.edu.pk>
2023-04-28 22:05:03 +0500

    Banner-created

--- app/donate/page.jsx
+++ app/donate/page.jsx
@@ -3,15 +3,14 @@ import Navbar from "../global-components/Navbar";
 import Footer from "../global-components/Footer";
 import Signin from "../global-components/Signin";
 import Signup from "../global-components/Signup";
+import Modal from "../Components/banner";
 
 const Donate = () => {
   return (
     <>
-      <Navbar />
-      <div>
-        <h1>Hello</h1>
+      <div className="w-screen h-screen flex items-center justify-center">
+        <Modal />
       </div>
-      <Footer />
     </>
   );
 };
