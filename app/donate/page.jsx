d85be64d45300c5cb0937ad940cd82335ef2964a
HussainSyed268 <snaqvi.bscs21seecs@seecs.edu.pk>
2023-04-28 19:08:39 +0500

    donate-folder

---
+++ app/donate/page.jsx
@@ -0,0 +1,18 @@
+import Main from "../index-components/Main";
+import Navbar from "../global-components/Navbar";
+import Footer from "../global-components/Footer";
+import Signin from "../global-components/Signin";
+import Signup from "../global-components/Signup";
+
+const Donate = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>Hello</h1>
+      </div>
+      <Footer />
+    </>
+  );
+};
+export default Donate;
