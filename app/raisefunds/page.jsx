5c9247ad954a39768acf72ecf1029e815cd71717
HussainSyed268 <snaqvi.bscs21seecs@seecs.edu.pk>
2023-04-29 16:42:01 +0500

    raise-funds adding inputs

--- app/raisefunds/page.jsx
+++ app/raisefunds/page.jsx
@@ -1,28 +1,47 @@
+"use client"
+import { useState } from "react";
 const RaiseFunds = () => {
+  const [reason1,setReason] = useState("");
+  const [form,setForm] = useState({
+    reason:""
+  });
+  function onSubmit(){
+    setForm({...form.reason=reason1});
+  }
   return (
     <>
       <div className="h-screen w-screen mt-8 flex bg-slate-400 rounded-l-3xl">
         <div className="w-2/6 flex flex-col justify-center items-center">
-        <h2 className="text-3xl font-medium tracking-tight text-slate-700 sm:text-4xl mb-8">Let's lend a hand to someone,</h2>
-        <h2 className="text-3xl font-semi-bold tracking-tight text-blue-900 sm:text-5xl">who really needs it.</h2>
+        <h2 className="text-3xl font-medium tracking-tight text-slate-700 sm:text-4xl mb-8 tracking-tighter">Let's lend a hand to someone,</h2>
+        <h2 className="text-3xl font-semi-bold tracking-tight text-blue-900 sm:text-5xl tracking-tighter">who really needs it.</h2>
         </div>
         <div className="w-4/6 bg-slate-300 rounded-l-3xl shadow-xl">
-        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-<div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-      </li>
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-      </li>
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-      </li>
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-      </li>
-    </ul>
+        <div className="flex flex-col mt-24 ml-24 h-5/6">
+        <div className="flex">
+  <label htmlFor="reason" className=" font-normal text-lg mr-6 mt-2">Select a reason for collecting funds:</label>
+  <select id="reason" name="reason" className="h-11 dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+    <option value="emergency" onClick={()=>setReason("emergency")}>Emergency</option>
+    <option value="education" onClick={()=>setReason("education")}>Education</option>
+    <option value="environment" onClick={()=>setReason("environment")}>Healthcare</option>
+  </select>
+</div>
+<div className="flex mt-11">
+<h1 className="font-normal text-lg mt-1 ">Identity of donee:</h1>
+<div className="flex ml-6">
+<div className="form-control">
+  <label className="label cursor-pointer">
+    <span className="label-text mr-4">Anonymous</span> 
+    <input type="radio" name="radio-10" className="radio checked:bg-red-700" checked />
+  </label>
+</div>
+<div className="form-control">
+  <label className="label cursor-pointer">
+    <span className="label-text mr-4">Visible</span> 
+    <input type="radio" name="radio-10" className="radio checked:bg-blue-700" checked />
+  </label>
+</div>
+</div>
+</div>
 </div>
 
         </div>
