4d4c0cf3786c9256c46564ccd2a0e4efeca64eef
Muhammad Taha Mukhtar <91777330+tahamukhtar20@users.noreply.github.com>
2023-04-29 11:46:51 +0500

    Routine Push

--- app/profile/components/CurrentSettingComponent.jsx
+++ app/profile/components/CurrentSettingComponent.jsx
@@ -1,5 +1,5 @@
 export default function CurrentSettingComponent(props) {
   return (
-    <div className="group grid  min-w-full place-items-center rounded-xl p-10 py-6 shadow-lg"></div>
+    <div className="group grid  h-screen min-w-full place-items-center rounded-xl p-10 py-6 shadow-lg"></div>
   );
 }
--- app/profile/page.js
+++ app/profile/page.js
@@ -5,14 +5,14 @@ import CurrentSettingComponent from "./components/CurrentSettingComponent";
 export default function Page() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-12">
-      <div className="col-span-1 bg-white md:col-span-8">
+      <div className="col-span-1 bg-white md:col-span-9">
         <div className="m-6 h-screen">
           <div className="inline">
             <CurrentSettingComponent />
           </div>
         </div>
       </div>
-      <div className="col-span-1 bg-white md:col-span-4">
+      <div className="col-span-1 bg-white md:col-span-3">
         <div className="m-6 flex flex-col">
           <ProfileComponent />
           <ButtonGroupComponent />
