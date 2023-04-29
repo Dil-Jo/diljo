7134dcb50e1dddbf022044d2f4dca8bac7a8f666
Muhammad Taha Mukhtar <91777330+tahamukhtar20@users.noreply.github.com>
2023-04-29 18:03:02 +0500

    Routine Push

--- app/profile/components/CurrentSettingComponent.jsx
+++ app/profile/components/CurrentSettingComponent.jsx
@@ -1,25 +1,46 @@
 import Image from "next/image";
+import Button from "../../global-components/Button";
 
 export default function CurrentSettingComponent(props) {
   return (
     <div className="group grid place-items-center rounded-xl p-10 shadow-lg">
+      <div className={"w-full"}>
+        <h1
+          className={
+            "text m-8 mb-12 inline w-72 text-start text-3xl font-black tracking-tighter text-gray-800"
+          }
+        >
+          About
+        </h1>
+      </div>
       <div className={"h-[50vh] w-full"}>
-        <div className={"m-5"}>
-          <div>
-            <h1>Name</h1>
-            <p>{props.name}</p>
-            <div
-              className={"w-full border-b-2 border-dashed border-black"}
-            ></div>
-            <Image
-              alt={""}
-              src={"/assets-profile/pencil.png"}
-              width={10}
-              height={10}
-            ></Image>
-          </div>
-        </div>
+        <Field name="fuckyouhfjshalfdlj" detail={"jfdsalkjflakjflkajflajf"} />
+        <Field name="fuckyouhfjshalfdlj" detail={"jfdsalkjflakjflkajflajf"} />
+        <Field name="fuckyouhfjshalfdlj" detail={"jfdsalkjflakjflkajflajf"} />
+        <Field name="fuckyouhfjshalfdlj" detail={"jfdsalkjflakjflkajflajf"} />
       </div>
     </div>
+  );
+}
+
+function Field(props) {
+  return (
+    <>
+      <div className={"m-5 border-b-2"}>
+        <div className={"m-3 flex"}>
+          <h1
+            className={
+              "text inline w-72  text-xl font-black tracking-tighter text-gray-900"
+            }
+          >
+            {props.name}
+          </h1>
+          <h1 className={"text inline w-[50rem] text-lg  text-gray-500"}>
+            {props.detail}
+          </h1>
+          <Button type={"primary"} text={"edit"}></Button>
+        </div>
+      </div>
+    </>
   );
 }
