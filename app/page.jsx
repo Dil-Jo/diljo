94890d16078787feb3177fd6cf2188aa71738830
Muhammad Taha Mukhtar <91777330+tahamukhtar20@users.noreply.github.com>
2023-04-29 07:12:17 +0500

    Bug Fix Complete

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,11 @@ import FeaturedPosts from "./index-components/FeaturedPosts";
 import About from "./index-components/About";
 import Main from "./index-components/Main";
 import Navbar from "./global-components/Navbar";
-import Footer from "./global-components/Footer";
-import Donate from "./global-components/Donate";
 
 export default function Page() {
   return (
     <>
+      <Navbar />
       <Main />
       <div className="mx-4 mt-6">
         <FeaturedPosts />
