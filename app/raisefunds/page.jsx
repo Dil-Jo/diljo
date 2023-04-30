b4390dc7eac27d7a2b0d90242a27cb330f6f9159
Muhammad Taha Mukhtar <91777330+tahamukhtar20@users.noreply.github.com>
2023-04-30 15:03:31 +0500

    hello

--- app/raisefunds/page.jsx
+++ app/raisefunds/page.jsx
@@ -1,11 +1,12 @@
 import Comp1 from "./Components/Raisefund1.jsx";
 import Comp2 from "./Components/Raisefund2.jsx";
+
 const RaiseFunds = () => {
   return (
     <>
 
-      {/* <Comp1 /> */}
-      <Comp2 />
+      <Comp1 />
+      {/*<Comp2 />*/}
     </>
   );
 };
