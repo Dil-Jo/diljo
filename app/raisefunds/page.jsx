8d9762f17ad6c77f01100ee36428e25ab6442e53
Shameekh <shameekh2002@gmail.com>
2023-05-01 00:27:55 +0500

    pb add fundraiser setup complete

--- app/raisefunds/page.jsx
+++ app/raisefunds/page.jsx
@@ -1,27 +1,58 @@
 'use client'
 import { useEffect, useState } from "react";
+import PocketBase from 'pocketbase';
 import Form1 from "./Components/Form1.jsx";
 import Form2 from "./Components/Raisefund1.jsx";
 import Form3 from "./Components/Raisefund2.jsx";
+
+
+async function submitForm(data) {
+  const pb = new PocketBase('http://127.0.0.1:8090');
+  const record = await pb.collection('fundraisers').create(data);
+  return record;
+}
+
 const RaiseFunds = () => {
   const [stage, setStage] = useState(1);
   const [fullForm, setFullForm] = useState({});
-  const updateForm = (form) => {
+  const updateForm = async (form) => {
     setFullForm({ ...fullForm, ...form })
+    console.log({ stage })
+    if (stage === 4) { //Submit the data to the server
+      // temporary user 
+      const user = {
+        id: 'fsg3aqlbwbfkn7q'
+      }
+      const data = {
+        title: fullForm.title,
+        caption: fullForm.description,
+        target: fullForm.amount,
+        owner: user.id,
+        coverPhoto: fullForm.image,
+        anonanonymityStatus: fullForm['radio-10'],
+        category: fullForm.reason,
+        finalDate: '2025-12-1'
+      }
+      console.log({ data })
+
+      const response = await submitForm(data);
+      if (response.id) alert('Form submitted successfully');
+      else alert('Form submission failed');
+
+    }
   }
+  useEffect(() => {
+    updateForm({})
+  }, [stage])
   // const [messages, setMessages] = useState([])
-  const [reason1, setReason] = useState("");
-  const [form, setForm] = useState({
-    reason: ""
-  });
+  // const [reason1, setReason] = useState("");
+  // const [form, setForm] = useState({
+  //   reason: ""
+  // });
   // function onSubmit() {
   //   setForm({ ...form.reason = reason1 });
   // }
 
-  useEffect(() => {
-    if (stage === 4)
-      console.log(fullForm);
-  }, [fullForm])
   return (
     <div className="h-screen w-screen mt-8 flex bg-slate-400 rounded-l-3xl">
       <div className="w-2/6 flex flex-col justify-center items-center">
@@ -29,8 +60,6 @@ const RaiseFunds = () => {
       </div>
       <div className="w-4/6 bg-slate-300 rounded-l-3xl shadow-xl">
         {true && <Form updateForm={updateForm} setStage={setStage} stage={stage} />}
-        {/* {stage > 3 && <Finish />} */}
-
       </div>
     </div>
   )
