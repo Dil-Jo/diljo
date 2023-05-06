90d80b8ffac19dd90c185cadee62236ff9271e72
Muhammad Taha Mukhtar <91777330+tahamukhtar20@users.noreply.github.com>
2023-05-06 13:56:49 +0500

    Footer Fixed

--- app/global-components/Footer.jsx
+++ app/global-components/Footer.jsx
@@ -4,40 +4,42 @@ import { usePathname } from "next/navigation";
 
 function ConditionalFooter() {
   return (
-    <footer className="m-4 rounded-lg bg-gray-200 shadow dark:bg-gray-900">
-      <div className="mx-auto w-full max-w-screen-xl p-4 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <div>
-            <h1>logo</h1>
-          </div>
-          <ul className="mb-6 flex flex-wrap items-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:mb-0">
-            <li>
-              <Link
-                href="/explore"
-                className=" my-10 ml-10 text-gray-700 hover:underline dark:text-gray-400 md:text-lg"
-              >
-                Explore
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/main"
-                className=" my-10 ml-10 text-gray-700 hover:underline dark:text-gray-400 md:text-lg"
-              >
-                Main
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/about"
-                className=" my-10 ml-10 text-gray-700 hover:underline dark:text-gray-400 md:text-lg"
-              >
-                About
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
+    <footer className="m-4 rounded-lg bg-gray-200 shadow dark:bg-gray-900 p-1">
+      <div className="flex w-full flex-row items-center justify-around p-5 [&>ul]:border-gray-300 [&>ul]:border-l-2 [&>ul]:p-3 rounded-lg border-2 border-gray-300">
+        <h1>logo</h1>
+        <ul className="flex flex-col [&>li]:my-2">
+          <li>
+            <p>some detail</p>
+          </li>
+          <li>
+            <p>some detail</p>
+          </li>
+          <li>
+            <p>some detail</p>
+          </li>
+        </ul>
+        <ul className=" flex flex-col [&>li]:my-2">
+          <li>
+            <p>some detail</p>
+          </li>
+          <li>
+            <p>some detail</p>
+          </li>
+          <li>
+            <p>some detail</p>
+          </li>
+        </ul>
+        <ul className=" flex flex-col [&>li]:my-2">
+          <li>
+            <p>some detail</p>
+          </li>
+          <li>
+            <p>some detail</p>
+          </li>
+          <li>
+            <p>some detail</p>
+          </li>
+        </ul>
       </div>
     </footer>
   );
