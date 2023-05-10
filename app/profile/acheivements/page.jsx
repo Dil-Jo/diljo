1fec4b7271785eb6d8b669f306711a62179593de
Muhammad Taha Mukhtar <91777330+tahamukhtar20@users.noreply.github.com>
2023-05-10 13:14:39 +0500

    Acheivements work

--- app/profile/acheivements/page.jsx
+++ app/profile/acheivements/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import WideCard from "../components/WideCard";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function page() {
   const [Items, setItems] = useState([0]);
@@ -10,16 +11,16 @@ export default function page() {
   return (
     <>
       <div className="group grid place-items-center rounded-xl p-10 shadow-lg">
-        <div className={"w-full"}>
+        <div className={"w-full "}>
           <h1
             className={
-              "text m-8 mb-12 inline w-72 text-start text-3xl font-black tracking-tighter text-gray-800 lg:w-auto"
+              "text m-8 mb-12 inline  text-start text-3xl font-black tracking-tighter text-gray-800 lg:w-auto"
             }
           >
             Achievements
           </h1>
         </div>
-        <div className={"grid grid-cols-4 gap-4 w-full h-full"}>
+        <div className={"grid grid-cols-4 w-full h-full"}>
           {Items.map(() => (<AchievementsCard />))}
         </div>
       </div>
@@ -27,13 +28,52 @@ export default function page() {
 
 }
 
-function AchievementsCard() {
-  return <>
-    <div className={"w-full h-full"}>
-      <div className={"border-2 border-gray-300 rounded-xl h-52"}>
-        <Image src={""}></Image>
-      </div>
-    </div>
+function AchievementsCard(props) {
+  const [isHovered, setIsHovered] = useState(false);
 
-  </>;
+  const handleHover = () => {
+    setIsHovered(true);
+  };
+
+  const handleHoverOut = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <>
+      <div className="w-full h-full mb-12">
+        <div
+          style={{
+            background:
+              "linear-gradient(109.6deg, #4C7D94 11.2%, #6A4E67 53.7%, #FCF9EE 100.2%)"
+          }}
+          className="border-2 border-gray-300 rounded-xl h-72 w-72 hover:scale-105 transition-all ease-in"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleHoverOut}
+        >
+          <div className={"w-full h-full rounded-xl hover:bg-black hover:bg-opacity-30 transition-all ease-linear"}>
+            {isHovered ? (
+              <div className="flex flex-row justify-center items-center h-full w-full">
+                <p className="text-white text-xl">{props.description}</p>
+              </div>
+            ) : (
+              <div className="hover:bg-black w-full h-full rounded-xl hover:bg-opacity-20 transition-all ease-in-out">
+                <div className="flex flex-row justify-center items-center h-full w-full">
+                  <Image
+                    width={144}
+                    height={144}
+                    src={props.image}
+                    style={{ objectFit: "cover" }}
+                    className="transition-all ease-in-out"
+                    loading="lazy"
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-	],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-			},
-		},
-	},
-	plugins: [require('daisyui')],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#fcf9ee",
+        "secondary": "#4c7d94",
+        "tertiary": "#6a4e67",
+        "quaternary": "#1e212b"
+      },
+
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      }
+    }
+  },
+  plugins: [require("daisyui")]
 };
