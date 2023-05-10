94bc5dc9f8dc931d0918e245917d7db4ddbb35e4
Muhammad Taha Mukhtar <91777330+tahamukhtar20@users.noreply.github.com>
2023-05-10 17:15:03 +0500

    acheivements functionality done

--- app/profile/acheivements/page.jsx
+++ app/profile/acheivements/page.jsx
@@ -1,13 +1,30 @@
 "use client";
-import WideCard from "../components/WideCard";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import PocketBase from "pocketbase";
 
 export default function page() {
-  const [Items, setItems] = useState([0]);
+  let data = JSON.parse(localStorage.getItem("Login")).record;
+  let pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
+  const [ItemsAll, setItemsAll] = useState([]);
+  const getAchievements = async () => {
+    const filter = `user_id = "${data.id}"`;
+
+    const records = await pb.collection("acheivement_users").getFullList(200, { filter });
+    const Items = records.map(record => record.acheivement_id);
+
+    const records2 = await pb.collection("acheivements").getFullList(200, {});
+    setItemsAll(records2.map(record => {
+      record.acheived = Items.includes(record.id);
+      return record;
+    }));
+
+  };
+
   useEffect(() => {
-    setItems([1, 23, 4, 5, 6, 7, 8, 9, 0, 7, 8]);
+    getAchievements();
   }, []);
+
   return (
     <>
       <div className="group grid place-items-center rounded-xl p-10 shadow-lg">
@@ -21,7 +38,7 @@ export default function page() {
           </h1>
         </div>
         <div className={"grid grid-cols-4 w-full h-full"}>
-          {Items.map(() => (<AchievementsCard />))}
+          {ItemsAll.map((item) => (<AchievementsCard {...item} key={item.id} />))}
         </div>
       </div>
     </>);
@@ -29,12 +46,11 @@ export default function page() {
 }
 
 function AchievementsCard(props) {
+  let pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
   const [isHovered, setIsHovered] = useState(false);
-
   const handleHover = () => {
     setIsHovered(true);
   };
-
   const handleHoverOut = () => {
     setIsHovered(false);
   };
@@ -47,26 +63,30 @@ function AchievementsCard(props) {
             background:
               "linear-gradient(109.6deg, #4C7D94 11.2%, #6A4E67 53.7%, #FCF9EE 100.2%)"
           }}
-          className="border-2 border-gray-300 rounded-xl h-72 w-72 hover:scale-105 transition-all ease-in"
+          className={`border-2 border-gray-300 rounded-xl h-72 w-72 hover:scale-105 transition-all ease-in ${props.acheived ? "opacity-100" : "opacity-75 filter grayscale"}`}
           onMouseEnter={handleHover}
           onMouseLeave={handleHoverOut}
         >
-          <div className={"w-full h-full rounded-xl hover:bg-black hover:bg-opacity-30 transition-all ease-linear"}>
-            {isHovered ? (
+          <div
+            className={`w-full h-full rounded-xl transition-all ease-linear z-50  hover:bg-black hover:bg-opacity-30`}>
+            {(isHovered && props.acheived) ? (
               <div className="flex flex-row justify-center items-center h-full w-full">
-                <p className="text-white text-xl">{props.description}</p>
+                <p className="text-white">{props.description}</p>
               </div>
             ) : (
               <div className="hover:bg-black w-full h-full rounded-xl hover:bg-opacity-20 transition-all ease-in-out">
-                <div className="flex flex-row justify-center items-center h-full w-full">
-                  <Image
-                    width={144}
-                    height={144}
-                    src={props.image}
-                    style={{ objectFit: "cover" }}
-                    className="transition-all ease-in-out"
-                    loading="lazy"
-                  />
+                <div className={"flex flex-col justify-center items-center h-full w-full"}>
+                  <div className={"h-52 w-52 flex justify-center items-center"}>
+                    <Image
+                      width={144}
+                      height={144}
+                      src={pb.files.getUrl(props, props.image)}
+                      style={{ objectFit: "cover" }}
+                      className="transition-all ease-in-out z-0"
+                      loading="lazy"
+                    />
+                  </div>
+                  <h1 className={"text-center text-3xl font-bold text-gray-50"}>{props.name}</h1>
                 </div>
               </div>
             )}
