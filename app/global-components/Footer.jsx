527bcf805438b937c74e1cea7a4ce61394b9e6d1
HussainSyed268 <snaqvi.bscs21seecs@seecs.edu.pk>
2023-05-11 10:39:45 +0500

    wanna smash

--- app/global-components/Footer.jsx
+++ app/global-components/Footer.jsx
@@ -1,18 +1,26 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import Logo from "./Logo";
 function ConditionalFooter() {
   return (
     <footer className="m-4 rounded-lg bg-gray-200 shadow dark:bg-gray-900 p-1">
       <div className="grid md:grid-cols-4 grid-cols-1 gap-4  w-full p-5 [&>ul]:border-gray-300 [&>ul]:border-l-2 [&>ul]:p-3 rounded-lg border-2 border-gray-300">
-        <h1>logo here</h1>
+        <div className="w-20 ml-32">
+          <Link href="/">
+            <Logo width="80px" />
+          </Link>
+        </div>
         <ul className="flex flex-col [&>li]:my-2">
           <li>
-            <p>some detail</p>
+            <Link href="/explore">
+              <p>Explore Fundraisers</p>
+            </Link>
           </li>
           <li>
-            <p>some detail</p>
+            <Link href="/raisefunds">
+              <p>Start a Fundraiser</p>
+            </Link>
           </li>
           <li>
             <p>some detail</p>
