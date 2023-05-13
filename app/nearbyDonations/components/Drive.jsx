5c5bc5a21082d7b775bbe35fe99ef406c036fb85
Muhammad Taha Mukhtar <91777330+tahamukhtar20@users.noreply.github.com>
2023-05-13 11:02:12 +0500

    Nearby Donations Update

--- app/nearbyDonations/components/Drive.jsx
+++ app/nearbyDonations/components/Drive.jsx
@@ -51,7 +51,7 @@ const Drive = ({
 
 	return (
 		<div
-			className={`shadow-lg flex border transition-all border-solid border-black rounded-xl mb-[1rem] active:bg-blue-900 active:text-white ${
+			className={`shadow-lg flex border transition-all border-solid border-black rounded-xl mb-[1rem] active:bg-blue-900 active:text-white cursor-pointer${
 				stautsState ? 'border-green-500' : ''
 			}`}
 			onClick={(e) => {
@@ -60,12 +60,12 @@ const Drive = ({
 			}}
 		>
 			<div className='flex flex-col w-5/6'>
-				<h3 className='text-[1.3rem] font-normal ml-4 mt-4'>
-					Donation Name: {title}
+				<h3 className='text-[1.3rem] font-bold underline ml-4 mt-4'>
+					{title}
 				</h3>
 				<div className='flex flex-row ml-4 mt-2'>
 					<h3 className='text-[1.3rem] font-normal mb-3'>
-						Category: {String(category).toUpperCase()}
+						<span className={"font-semibold"}>For</span> {" "} {String(category).toUpperCase()}
 					</h3>
 					<h3 hidden>Start Date: {stDate}</h3>
 					<h3 hidden>End Date: {endDate}</h3>
