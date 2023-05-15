dfa4b2f55a4e964ec5ccd85676b64c0e04bbbf94
Shameekh <shameekh2002@gmail.com>
2023-05-15 19:48:52 +0500

    nearby donations now have button disabled and laoding and stuff

--- app/nearbyDonations/components/Toast.jsx
+++ app/nearbyDonations/components/Toast.jsx
@@ -1,16 +1,21 @@
 export default function (props) {
-  let classes = ""
-  if (props.color == null) classes = 'bg-two';
-  else classes = `${props.color}`;
-  return (
-    <>
-      <div className="toast">
-        <div className={'rounded-3xl py-2 px-4'.concat(" ", classes)}>
-          <div>
-            <span className={"text-white"}>{props.text}</span>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	// let classes = ""
+	// if (props.color == null) classes = 'bg-two';
+	// else classes = `${props.color}`;
+	return (
+		<>
+			<div className='toast'>
+				<div
+					// className={`rounded-3xl py-2 px-4 alert alert-info ${
+					className={`alert alert-info font-semibold${
+						props.color == null ? 'bg-two' : props.color
+					}`}
+				>
+					<div>
+						<span className={'text-white'}>{props.text}</span>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
